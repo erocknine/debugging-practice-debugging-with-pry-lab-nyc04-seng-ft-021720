2e43f4de9e010b2796db911828a4ea058c88cb1b
Sir Eric <echeungsk@gmail.com>
2020-02-05 02:47:19 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,9 +11,9 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+  if num == 1
      puts "YUM YUM MUNCH MUNCH MUNCH"
-  else if num = 2
+  else if num == 2
      puts "HAM HAM HAM IN MY TUMMY"
   end
 end
